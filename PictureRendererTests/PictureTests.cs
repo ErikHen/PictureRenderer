3f24bc4ac29d84a712015aeda43a934df72ff654
Erik Henningson <ErikHen@users.noreply.github.com>
2021-11-22 20:45:35 +0100

    Update tests

--- PictureRendererTests/PictureTests.cs
+++ PictureRendererTests/PictureTests.cs
@@ -14,7 +14,7 @@ namespace PictureRenderer.Tests
         [Fact()]
         public void RenderWithoutWebpTest()
         {
-            const string expected = "<picture><source srcset=\"/myImage.jpg?width=375&height=211&quality=80 375w, /myImage.jpg?width=750&height=422&quality=80 750w, /myImage.jpg?width=980&height=551&quality=80 980w, /myImage.jpg?width=1500&height=844&quality=80 1500w\" sizes=\"(max-width: 980px) calc((100vw - 40px)), (max-width: 1200px) 368px, 750px\" /><img alt=\"\" src=\"/myImage.jpg?width=1500&height=844&quality=80\" loading=\"lazy\"/></picture>";
+            const string expected = "<picture><source srcset=\"/myImage.jpg?width=375&height=211&quality=80 375w, /myImage.jpg?width=750&height=422&quality=80 750w, /myImage.jpg?width=980&height=551&quality=80 980w, /myImage.jpg?width=1500&height=844&quality=80 1500w\"sizes=\"(max-width: 980px) calc((100vw - 40px)), (max-width: 1200px) 368px, 750px\"/><img alt=\"\"src=\"/myImage.jpg?width=1500&height=844&quality=80\"width=\"1500\"height=\"844\"loading=\"lazy\"decoding=\"async\"/></picture>";
 
             var profile = new ImageSharpProfile()
             {
@@ -32,7 +32,7 @@ namespace PictureRenderer.Tests
         [Fact()]
         public void RenderWithWebpTest()
         {
-            const string expected = "<picture><source srcset=\"/myImage.jpg?format=webp&width=150&height=150&quality=80 150w, /myImage.jpg?format=webp&width=300&height=300&quality=80 300w\" sizes=\"150px\" type=\"image/webp\"/><source srcset=\"/myImage.jpg?width=150&height=150&quality=80 150w, /myImage.jpg?width=300&height=300&quality=80 300w\" sizes=\"150px\" /><img alt=\"alt text\" src=\"/myImage.jpg?width=300&height=300&quality=80\" loading=\"lazy\"/></picture>";
+            const string expected = "<picture><source srcset=\"/myImage.jpg?format=webp&width=150&height=150&quality=80 150w, /myImage.jpg?format=webp&width=300&height=300&quality=80 300w\"sizes=\"150px\"type=\"image/webp\"/><source srcset=\"/myImage.jpg?width=150&height=150&quality=80 150w, /myImage.jpg?width=300&height=300&quality=80 300w\"sizes=\"150px\"/><img alt=\"alt text\"src=\"/myImage.jpg?width=300&height=300&quality=80\"width=\"300\"height=\"300\"loading=\"lazy\"decoding=\"async\"/></picture>";
 
             var profile = new ImageSharpProfile()
             {
@@ -48,19 +48,39 @@ namespace PictureRenderer.Tests
         }
 
         [Fact()]
-        public void RenderWithCssClass()
+        public void RenderWithCssClassAndImageDecodingAuto()
         {
-            const string expected = "<picture><source srcset=\"/myImage.jpg?format=webp&width=150&height=150&quality=80 150w, /myImage.jpg?format=webp&width=300&height=300&quality=80 300w\" sizes=\"150px\" type=\"image/webp\"/><source srcset=\"/myImage.jpg?width=150&height=150&quality=80 150w, /myImage.jpg?width=300&height=300&quality=80 300w\" sizes=\"150px\" /><img alt=\"alt text\" src=\"/myImage.jpg?width=300&height=300&quality=80\" loading=\"lazy\"class=\"my-css-class\"/></picture>";
+            const string expected = "<picture><source srcset=\"/myImage.jpg?format=webp&width=150&height=150&quality=80 150w, /myImage.jpg?format=webp&width=300&height=300&quality=80 300w\"sizes=\"150px\"type=\"image/webp\"/><source srcset=\"/myImage.jpg?width=150&height=150&quality=80 150w, /myImage.jpg?width=300&height=300&quality=80 300w\"sizes=\"150px\"/><img alt=\"alt text\"src=\"/myImage.jpg?width=300&height=300&quality=80\"width=\"300\"height=\"300\"loading=\"lazy\"decoding=\"auto\"class=\"my-css-class\"/></picture>";
+            var profile = new ImageSharpProfile()
+            {
+                SrcSetWidths = new[] { 150, 300 },
+                Sizes = new[] { "150px" },
+                AspectRatio = 1,
+                ImageDecoding = ImageDecoding.Auto,
+            };
+
+
+            var result = PictureRenderer.Picture.Render("/myImage.jpg", profile, "alt text", "my-css-class");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact()]
+        public void RenderWithoutWidthHeightAndDecoding()
+        {
+            const string expected = "<picture><source srcset=\"/myImage.jpg?format=webp&width=150&height=150&quality=80 150w, /myImage.jpg?format=webp&width=300&height=300&quality=80 300w\"sizes=\"150px\"type=\"image/webp\"/><source srcset=\"/myImage.jpg?width=150&height=150&quality=80 150w, /myImage.jpg?width=300&height=300&quality=80 300w\"sizes=\"150px\"/><img alt=\"alt text\"src=\"/myImage.jpg?width=300&height=300&quality=80\"loading=\"lazy\"/></picture>";
 
             var profile = new ImageSharpProfile()
             {
                 SrcSetWidths = new[] { 150, 300 },
                 Sizes = new[] { "150px" },
-                AspectRatio = 1
+                AspectRatio = 1,
+                NoImgWidthHeight = true,
+                ImageDecoding = ImageDecoding.None,
             };
 
 
-            var result = PictureRenderer.Picture.Render("/myImage.jpg", profile, "alt text", "my-css-class");
+            var result = PictureRenderer.Picture.Render("/myImage.jpg", profile, "alt text");
 
             Assert.Equal(expected, result);
         }
